323902eb52410dddac3f1c680efbd2b0527a2efe
Maxwell Giuliano <giulianomaxwell@outlook.com>
2023-03-19 23:24:56 -0400

    First commit.

---
+++ src/main/java/dev/power/BalancedMinecraft.java
@@ -0,0 +1,10 @@
+package dev.power;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BalancedMinecraft implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
