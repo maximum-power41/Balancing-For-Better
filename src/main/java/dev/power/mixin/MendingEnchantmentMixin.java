44a2c15fa0e8368d12b1bf14167ba12c212ce727
Maxwell Giuliano <giulianomaxwell@outlook.com>
2023-03-19 23:31:53 -0400

    Added Mending Enchantment Mixin.

---
+++ src/main/java/dev/power/mixin/MendingEnchantmentMixin.java
@@ -0,0 +1,9 @@
+package dev.power.mixin;
+
+import net.minecraft.enchantment.MendingEnchantment;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(MendingEnchantment.class)
+public abstract class MendingEnchantmentMixin {
+
+}
