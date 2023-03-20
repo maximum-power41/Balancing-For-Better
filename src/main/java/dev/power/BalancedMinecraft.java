198de8aae2203fa28a791ef587044f6009d48d04
Maxwell Giuliano <giulianomaxwell@outlook.com>
2023-03-20 02:14:15 -0400

    Added Mixin to remove Mending enchantment from Villager trades. Added Logger and Mod ID.

--- src/main/java/dev/power/BalancedMinecraft.java
+++ src/main/java/dev/power/BalancedMinecraft.java
@@ -1,8 +1,14 @@
 package dev.power;
 
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BalancedMinecraft implements ModInitializer {
+
+    public static final String MOD_ID = "balanced-minecraft";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
     @Override
     public void onInitialize() {
 
--- src/main/java/dev/power/mixin/MendingEnchantmentMixin.java
+++
@@ -1,9 +0,0 @@
-package dev.power.mixin;
-
-import net.minecraft.enchantment.MendingEnchantment;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(MendingEnchantment.class)
-public abstract class MendingEnchantmentMixin {
-
-}
